33e0f66f63364aa9f8588a4cb5b653dc94b8dfcc
Aadhil-M <muhammadaadhil11@gmail.com>
2026-06-09 17:49:14 +0530

    Add configuration header for WiFi and MQTT settings

---
+++ src/config.h
@@ -0,0 +1,10 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#define WIFI_SSID     "YOUR_WIFI"
+#define WIFI_PASSWORD "YOUR_PASSWORD"
+
+#define MQTT_SERVER   "broker.hivemq.com"
+#define MQTT_PORT     1883
+
+#endif
